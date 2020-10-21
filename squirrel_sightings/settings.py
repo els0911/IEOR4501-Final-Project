ec1e76d5434732cfc124c3c01fa0b6eb2b26a39e
Els Dai <ed2912@columbia.edu>
2020-10-21 04:13:31 +0000

    Create map file

--- squirrel_sightings/settings.py
+++ squirrel_sightings/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'map',
 ]
 
 ROOT_URLCONF = 'squirrel_sightings.urls'
